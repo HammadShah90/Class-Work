678f3070449e370c25eacba16f2c2f534fb9e6bf
HammadShah90 <hammad.shaha90@gmail.com>
2023-03-22 23:44:11 +0500

    highOrder Function / callBack Function

---
+++ app.js
@@ -0,0 +1,38 @@
+// for (i = 0; i < 100; ++i) {
+//     // iteration
+//     console.log(i);
+// }
+
+
+// Expression Function
+// function myFunk () {
+
+// }
+
+// Arrow Function
+// const myFunk = () => {
+
+// }
+
+// Anonymous Function
+// const myFunk = function() {
+
+// }
+
+// function meraFunk1(secondFunk) {
+//     let kisKiCallAyi = prompt('write the name?')
+
+//     secondFunk(kisKiCallAyi)
+// }
+
+// highOrder // callBack
+// meraFunk1(meraFunk2);
+
+// function meraFunk2(kisKiCallAyiThi) {
+//     console.log(kisKiCallAyiThi);
+// }
+
+
+let generalItems = ["colddrink", "shampoo", "pulses", "rice", "spices"]
+
+let 
